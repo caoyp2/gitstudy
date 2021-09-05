13d3e47839b300a815a1e7f3e9075fe10224069b
caoyp <caoyunpeng@yzw.cn>
2021-09-06 00:45:22 +0800

    打印ffffff

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("CCCCCCC");
         System.out.println("ddddddddddd");
+        System.out.println("fffffffffff");
     }
 }
